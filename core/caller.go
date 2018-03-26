76a73a3801868f8541140c6557d34bea3c01deac
XuQiaolun <Jamesxql@Gmail.com>
2018-03-26 14:03:14 +0800

    添加默认的zapcore.CallerEncoder

---
+++ core/caller.go
@@ -0,0 +1,13 @@
+package core
+
+import (
+	"runtime"
+	"strings"
+
+	"go.uber.org/zap/zapcore"
+)
+
+// DefaultCallerEncoder 默认的zapcore.CallerEncoder
+func DefaultCallerEncoder(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(strings.Join([]string{caller.TrimmedPath(), runtime.FuncForPC(caller.PC).Name()}, ":"))
+}
